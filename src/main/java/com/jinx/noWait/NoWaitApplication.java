ce3ea40305f15cf865ad656277e4ee2e6a8bfcbd
Pragadeesh <ww.pragat7@gmail.com>
2023-06-16 19:49:38 +0530

    spring init

---
+++ src/main/java/com/jinx/noWait/NoWaitApplication.java
@@ -0,0 +1,13 @@
+package com.jinx.noWait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoWaitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinx/noWait/NoWaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinx.noWait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoWaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
